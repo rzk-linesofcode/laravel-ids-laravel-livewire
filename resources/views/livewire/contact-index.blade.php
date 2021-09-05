e94129dfe9eeee081a1c483401c91ba5ea8f3ea3
RzK <rzk.linesofcode@gmail.com>
2021-09-05 21:35:05 +0700

    03-Shows-Contact-Data-Into-The-Table

--- resources/views/livewire/contact-index.blade.php
+++ resources/views/livewire/contact-index.blade.php
@@ -1,7 +1,31 @@
 <div>
-    <ul>
-        @foreach ($contacts as $contact)
-            <li>{{ $contact->name }}</li>
-        @endforeach
-    </ul>
+    <table class="table">
+        <thead class="table-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Phone</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            @php
+                $no =0;
+            @endphp
+            @foreach ($contacts as $contact)
+            @php
+                $no++;
+            @endphp
+                <tr>
+                    <th scope="row">{{ $no }}</th>
+                    <td>{{ $contact->name  }}</td>
+                    <td>{{ $contact->phone }}</td>
+                    <td>
+                        <button class="btn btn-sm btn-info text-white">Edit</button>
+                        <button class="btn btn-sm btn-danger text-white">Delete</button>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
