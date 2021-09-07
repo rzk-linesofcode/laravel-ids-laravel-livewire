13fd4b02b25727846b334e4ad59c03a4e3b88a17
RzK <rzk.linesofcode@gmail.com>
2021-09-07 14:20:20 +0700

    11-Pagination-in-Laravel

--- app/Http/Livewire/ContactIndex.php
+++ app/Http/Livewire/ContactIndex.php
@@ -4,9 +4,11 @@ namespace App\Http\Livewire;
 
 use App\Models\Contact;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ContactIndex extends Component
 {
+    use WithPagination;
 
     public $updateStatus = false;
 
@@ -18,7 +20,7 @@ class ContactIndex extends Component
     public function render()
     {
         return view('livewire.contact-index', [
-            'contacts' => Contact::latest()->get(),
+            'contacts' => Contact::latest()->paginate(5),
         ]);
     }
 
--- resources/views/livewire/contact-index.blade.php
+++ resources/views/livewire/contact-index.blade.php
@@ -44,4 +44,5 @@
             @endforeach
         </tbody>
     </table>
+    {{ $contacts->Links() }}
 </div>
